58027e694aa8cb366f0c007b5dcea9081180d181
SusuPelger <spelger@nevada.unr.edu>
2020-11-14 20:31:27 -0800

    added addresses

--- test.c
+++ test.c
@@ -1,1 +1,7 @@
-/idk this is a test
+//idk this is a test
+
+
+volatile unsigned char *my_ADMUX = (unsigned char*) 0x7C;
+volatile unsigned char *my_ADCSRB =  (unsigned char*) 0x7B;
+volatile unsigned char *my_ADCSRA = (unsigned char*) 0x7A;
+volatile unsigned int *my_ADC_DATA = (unsigned int*) 0x78;
