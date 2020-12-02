2ba9eab7b58054274c86e2bc98e4c81216dec0aa
SusuPelger <spelger@nevada.unr.edu>
2020-12-01 22:38:54 -0800

    added vent button - works!

--- buttontest/buttontest.ino
+++ buttontest/buttontest.ino
@@ -8,23 +8,31 @@ volatile unsigned char *pin_b = (unsigned char*) 0x23;
 
 void setup() {
     // put your setup code here, to run once:
-    *ddr_b &= 0xFD; //sets PB1 (enable button) to input 0b1111 1101
+    *ddr_b &= 0xFD; //sets PB1 (enable button) and PB3 (vent button) to input
     *ddr_b |= 0xF0; //sets LEDs as outputs
     *port_b &= 0x0F; //sets LEDs to low
-    *port_b &= 0xFD; //sets PB1 to low    
+    *port_b &= 0xF5; //sets PB1 and PB3 to low    
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
-    if((*pin_b & 0x02)) //if button pressed 0b0000 0010
+    if((*pin_b & 0x02)) //if enable button pressed
     {
         *port_b |= 0x40; //lights up green LED
         *port_b &= 0xEF; //red LED off
+        *port_b &= 0xDF; //yellow LED off
     }
-    else //button is not pressed
+    else if((*pin_b & 0x08)) //if vent button pressed
+    {
+        *port_b |= 0x20; //lights up yellow LED
+        *port_b &= 0xEF; //red LED off
+        *port_b &= 0xBF; //green LED off
+    }
+    else //buttons not pressed
     {
         *port_b |= 0x10; //lights up red LED
         *port_b &= 0xBF; //green LED off
+        *port_b &= 0xDF; //yellow LED off
     }
 
 }
