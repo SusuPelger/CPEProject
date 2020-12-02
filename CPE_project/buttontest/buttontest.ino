e7f89659c68c532b2e4cd2fb4868f0d9782a954c
SusuPelger <spelger@nevada.unr.edu>
2020-12-01 22:01:52 -0800

    button works (fixed pull-down), changed to PB1

--- CPE_project/buttontest/buttontest.ino
+++ CPE_project/buttontest/buttontest.ino
@@ -8,15 +8,15 @@ volatile unsigned char *pin_b = (unsigned char*) 0x23;
 
 void setup() {
     // put your setup code here, to run once:
-    *ddr_b &= 0xF7; //sets PB3 (enable button) to input 0b1111 0111
+    *ddr_b &= 0xFD; //sets PB1 (enable button) to input 0b1111 1101
     *ddr_b |= 0xF0; //sets LEDs as outputs
     *port_b &= 0x0F; //sets LEDs to low
-    *port_b &= 0xF7; //sets PB3 to low    
+    *port_b &= 0xFD; //sets PB1 to low    
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
-    if((*pin_b & 0x08)) //if button pressed 0b0000 1000
+    if((*pin_b & 0x02)) //if button pressed 0b0000 0010
     {
         *port_b |= 0x40; //lights up green LED
         *port_b &= 0xEF; //red LED off
